8e2745c5ccefb910394b78bbd18cf529b7ccc118
Solonarv <nstamm@gmx.de>
2019-02-17 07:15:13 +0100

    Re-export everything from Optics

--- lib/Optics.hs
+++ lib/Optics.hs
@@ -1,2 +1,11 @@
+module Optics (module E) where
 
-module Optics where
+import Optics.Base as E
+import Optics.Fold as E
+import Optics.Getter as E
+import Optics.Iso as E
+import Optics.Lens as E
+import Optics.Prism as E
+import Optics.Review as E
+import Optics.Setter as E
+import Optics.Traversal as E
